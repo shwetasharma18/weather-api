045fae95cfd3723fae846460666f71cfee2b0c84
Navgurukul-1 <shweta18@navgurukul.org>
2020-01-07 20:10:31 +0530

    some changes

--- weather.js
+++ weather.js
@@ -1,7 +1,8 @@
 var http = require("http");
+const readline = require('readline-sync');
 var weatherData = "";
 var APIKEY = "16a170cfed3fd644de1598cb146234b3";
-var city = "Chicago";
+var city = readline.question('Which city? :- ');;
 var options = {
 	host: 'api.openweathermap.org',
 	path: '/data/2.5/weather?q=' + city + '&APPID=' + APIKEY
@@ -10,9 +11,13 @@ var options = {
 http.request(options,function(weatheResponse){
 	weatheResponse.on("data",function(data){
 		weatherData += data;
+		let obj = JSON.parse(weatherData) 
+		// console.log(obj);
+		for(let i = 0; i < obj["weather"].length; i++){
+			id = obj["weather"][i]["id"]
+			// console.log(id)
+		console.log("lon :- " + obj.coord["lon"])
+		console.log("lat :- " + obj.coord["lat"])
+		}
 	})
-	weatheResponse.on("end",function(){
-		console.log(weatherData);
-	})
-}
-).end()
+}).end()
